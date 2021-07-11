8434125aec0b4d52b5fd77c15951de5acd3658a4
KuanyuPhy <spaul6627@gmail.com>
2021-07-12 01:57:44 +0800

    updaye code

--- ana/quick_draw.c
+++ ana/quick_draw.c
@@ -23,11 +23,11 @@ void quick_draw()
     float PT400CS = 0.3807;
     float PT650CS = 0.03711;
     auto c1 = new TCanvas("c", "BPRE");
-    TFile *Mx2_1 = new TFile("./../../root_file/test1nb.root");
-    TFile *Mx2_50 = new TFile("./../../root_file/test50nb.root");
-    TFile *Mx2_150 = new TFile("./../../root_file/test150nb.root");
+    TFile *Mx2_1 = new TFile("./../../root_file/test/test1.root");
+    TFile *Mx2_50 = new TFile("./../../root_file/test/test50.root");
+    TFile *Mx2_150 = new TFile("./../../root_file/test/test150.root");
 
-
+/*
     TFile *DYHT70 = new TFile("./../../root_file/tmpHT70_100.root");
     TFile *DYHT100 = new TFile("./../../root_file/tmpHT100_200.root");
     TFile *DYHT200 = new TFile("./../../root_file/tmpHT200_400.root");
@@ -36,49 +36,51 @@ void quick_draw()
     TFile *DYHT800 = new TFile("./../../root_file/tmpHT800_1200.root");
     TFile *DYHT1200 = new TFile("./../../root_file/tmpHT1200_2500.root");
     TFile *DYHT2500 = new TFile("./../../root_file/tmpHT2500_Inf.root");
+*/
+    TFile *DYpT50 = new TFile("./../../root_file/test/test_pt50.root");
+    TFile *DYpT100 = new TFile("./../../root_file/test/test_pt100.root");
+    TFile *DYpT250 = new TFile("./../../root_file/test/test_pt250.root");
+    TFile *DYpT400 = new TFile("./../../root_file/test/test_pt400.root");
+    TFile *DYpT650 = new TFile("./../../root_file/test/test_pt650.root");
 
-    TFile *DYpT50 = new TFile("./../../root_file/tmppt50_100.root");
-    TFile *DYpT100 = new TFile("./../../root_file/tmppt100_250.root");
-    TFile *DYpT250 = new TFile("./../../root_file/tmppt250_400.root");
-    TFile *DYpT400 = new TFile("./../../root_file/tmppt400_650.root");
-    TFile *DYpT650 = new TFile("./../../root_file/tmppt650_Inf.root");
-
-    TH1D *Mx2_1_nT = ((TH1D *)Mx2_1->Get("PUdz"));
-    TH1D *Mx2_50_nT = ((TH1D *)Mx2_50->Get("PUdz"));
-    TH1D *Mx2_150_nT = ((TH1D *)Mx2_150->Get("PUdz"));
+    TH1D *Mx2_1_nT = ((TH1D *)Mx2_1->Get("Chi3Dlog"));
+    TH1D *Mx2_50_nT = ((TH1D *)Mx2_50->Get("Chi3Dlog"));
+    TH1D *Mx2_150_nT = ((TH1D *)Mx2_150->Get("Chi3Dlog"));
 
     /*
     TH1D *HT_CS = ((TH1D *)DYHT70->Get("h_HTCS"));
-    TH1D *HT70_nT = ((TH1D *)DYHT70->Get("PUdz"));
+    TH1D *HT70_nT = ((TH1D *)DYHT70->Get("Chi3Dlog"));
     TH1D *HT70_evt = ((TH1D *)DYHT70->Get("h_event"));
 
-    TH1D *HT100_nT = ((TH1D *)DYHT100->Get("PUdz"));
+    TH1D *HT100_nT = ((TH1D *)DYHT100->Get("Chi3Dlog"));
     TH1D *HT100_evt = ((TH1D *)DYHT100->Get("h_event"));
 
-    TH1D *HT200_nT = ((TH1D *)DYHT200->Get("PUdz"));
+    TH1D *HT200_nT = ((TH1D *)DYHT200->Get("Chi3Dlog"));
     TH1D *HT200_evt = ((TH1D *)DYHT200->Get("h_event"));
-    TH1D *HT400_nT = ((TH1D *)DYHT400->Get("PUdz"));
+    TH1D *HT400_nT = ((TH1D *)DYHT400->Get("Chi3Dlog"));
     TH1D *HT400_evt = ((TH1D *)DYHT400->Get("h_event"));
-    TH1D *HT600_nT = ((TH1D *)DYHT600->Get("PUdz"));
+    TH1D *HT600_nT = ((TH1D *)DYHT600->Get("Chi3Dlog"));
     TH1D *HT600_evt = ((TH1D *)DYHT600->Get("h_event"));
-    TH1D *HT800_nT = ((TH1D *)DYHT800->Get("PUdz"));
+    TH1D *HT800_nT = ((TH1D *)DYHT800->Get("Chi3Dlog"));
     TH1D *HT800_evt = ((TH1D *)DYHT800->Get("h_event"));
-    TH1D *HT1200_nT = ((TH1D *)DYHT1200->Get("PUdz"));
+    TH1D *HT1200_nT = ((TH1D *)DYHT1200->Get("Chi3Dlog"));
     TH1D *HT1200_evt = ((TH1D *)DYHT1200->Get("h_event"));
-    TH1D *HT2500_nT = ((TH1D *)DYHT2500->Get("PUdz"));
+    TH1D *HT2500_nT = ((TH1D *)DYHT2500->Get("Chi3Dlog"));
     TH1D *HT2500_evt = ((TH1D *)DYHT2500->Get("h_event"));
     */
     //TH1D *h_PTCS = ((TH1D *)DYpT50->Get("h_PTCS"));
-    TH1D *DYPT50_nT = ((TH1D *)DYpT50->Get("PUdz"));
+    
+    TH1D *DYPT50_nT = ((TH1D *)DYpT50->Get("Chi3Dlog"));
     TH1D *DYPT50_sumW = ((TH1D *)DYpT50->Get("h_SumWeight"));
-    TH1D *DYPT100_nT = ((TH1D *)DYpT100->Get("PUdz"));
+    TH1D *DYPT100_nT = ((TH1D *)DYpT100->Get("Chi3Dlog"));
     TH1D *DYPT100_sumW = ((TH1D *)DYpT100->Get("h_SumWeight"));
-    TH1D *DYPT250_nT = ((TH1D *)DYpT250->Get("PUdz"));
+    TH1D *DYPT250_nT = ((TH1D *)DYpT250->Get("Chi3Dlog"));
     TH1D *DYPT250_sumW = ((TH1D *)DYpT250->Get("h_SumWeight"));
-    TH1D *DYPT400_nT = ((TH1D *)DYpT400->Get("PUdz"));
+    TH1D *DYPT400_nT = ((TH1D *)DYpT400->Get("Chi3Dlog"));
     TH1D *DYPT400_sumW = ((TH1D *)DYpT400->Get("h_SumWeight"));
-    TH1D *DYPT650_nT = ((TH1D *)DYpT650->Get("PUdz"));
+    TH1D *DYPT650_nT = ((TH1D *)DYpT650->Get("Chi3Dlog"));
     TH1D *DYPT650_sumW = ((TH1D *)DYpT650->Get("h_SumWeight"));
+    
     
 /*
     //double DYHT70_CS = HT_CS->GetBinContent(1);
@@ -115,8 +117,9 @@ void quick_draw()
     HT70_nT->Add(HT2500_nT);
 */
     //HT70_nT->GetXaxis()->SetRangeUser(0, 50);
-    //PUdz->GetXaxis()->SetRangeUser(0, 50)
+    //Chi3Dlog->GetXaxis()->SetRangeUser(0, 50)
     //double DYPT50_CS = h_PTCS->GetBinContent(1);
+
     double DYPT50_SW = DYPT50_sumW->GetBinContent(1);
     DYPT50_nT->Scale(PT50CS / DYPT50_SW);
     //double DYPT100_CS = h_PTCS->GetBinContent(2);
@@ -137,7 +140,15 @@ void quick_draw()
     DYPT50_nT->Add(DYPT400_nT);
     DYPT50_nT->Add(DYPT650_nT);
 
-    Mx2_1_nT->SetStats(0);
+    //int nBin = Mx2_50_nT->GetNbinsX();
+
+    Mx2_150_nT->SetLineStyle(7);
+    Mx2_1_nT->SetLineStyle(7);
+    Mx2_50_nT->SetLineStyle(7);
+
+    DYPT50_nT->SetLineStyle(7);
+
+    DYPT50_nT->SetStats(0);
     Mx2_1_nT->SetLineWidth(2);
     Mx2_1_nT->SetLineColor(kRed);
     Mx2_50_nT->SetLineWidth(2);
@@ -152,7 +163,7 @@ void quick_draw()
     DYPT50_nT->SetLineWidth(3);
     DYPT50_nT->SetLineColor(kViolet);
 
-    Mx2_1_nT->SetTitle("PUdz");
+    Mx2_1_nT->SetTitle("Chi3Dlog");
     //Mx2_1_nT->SetXTitle("NTracks per Jet");
     //Mx2_1_nT->SetXTitle("Numbers of Thin Jet");
     //Mx2_1_nT->GetXaxis()->SetBinLabel(1,"0");
@@ -167,22 +178,34 @@ void quick_draw()
     //Mx2_1_nT->GetXaxis()->SetBinLabel(10,"9");    
     //Mx2_1_nT->GetXaxis()->SetRangeUser(0, 5);
     //Mx2_1_nT->GetXaxis()->SetRangeUser(-2, 5);
-    //Mx2_1_nT->GetXaxis()->SetRangeUser(0, 8);
-    Mx2_1_nT->Draw("hist");
-    Mx2_50_nT->Draw("hist&&same");
-    Mx2_150_nT->Draw("hist&&same");
+    //
+    DYPT50_nT->SetXTitle("log_{10}(IP^{3D}_{sig})");
+    //Mx2_150_nT->GetXaxis()->SetRangeUser(-3, 5);
+   //DYPT50_nT->GetXaxis()->SetRangeUser(-1.5, 5);
+
+    DYPT50_nT->Draw("text90");
+    int nBin = Mx2_50_nT->GetNbinsX();
+    cout << nBin << endl;
+    double pp = DYPT50_nT->GetBinContent(75);
+    cout<<pp<<endl;
+    //Mx2_150_nT->DrawNormalized("hist&&same");
+    //Mx2_50_nT->DrawNormalized("hist&&same");
+    //Mx2_1_nT->DrawNormalized("hist&&same");
     //HT70_nT->Draw("hist&&same");
-    DYPT50_nT->Draw("hist&&same");
+    
+    //DYPT50_nT->DrawNormalized("hist&&same");
 
     TLegend *l2 = new TLegend(0.60, 0.55, 0.90, 0.90);
-    l2->AddEntry(Mx2_1_nT, "ctau=1mm m_{x^{2}}=1", "f");
-    l2->AddEntry(Mx2_150_nT, "ctau=1mm m_{x^{2}}=150", "f");
-    l2->AddEntry(Mx2_50_nT, "ctau=10mm m_{x^{2}}=50", "f");
+    l2->SetBorderSize(0);
+    l2->SetTextSize(0.04);
+    //l2->AddEntry(Mx2_1_nT, "ctau=1mm m_{x^{2}}=1", "l");
+    //l2->AddEntry(Mx2_150_nT, "ctau=1mm m_{x^{2}}=150", "l");
+    l2->AddEntry(Mx2_50_nT, "ctau=10mm m_{x^{2}}=50", "l");
 
     //l2->AddEntry(HT70_nT, "DYJets_HTBin", "f");
-    l2->AddEntry(DYPT50_nT, "DYJets_pTBin", "f");
+    l2->AddEntry(DYPT50_nT, "DYJets_pTBin", "l");
 
     l2->Draw();
-    c1->SetLogy();
-    //c1->SaveAs("PUdz.png");
+    //c1->SetLogy();
+    //c1->SaveAs("Chi3Dlog.png");
 }
